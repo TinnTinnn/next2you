d8914cb8d83bb2ae962ebf265fb6732373dd63fb
TinnTinnn <144338656+TinnTinnn@users.noreply.github.com>
2025-03-03 18:34:36 +0700

    Make component (Footer,Navigation,Navlink)
    Add logic style when that page is active

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,39 +1,28 @@
+"use client"
+
 import "./globals.css";
-import Link from "next/link";
 import {Poppins} from 'next/font/google'
-import {usePathname} from "next/navigation";
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
 
 const poppins = Poppins({
-    subsets: ["latin"],
-    variable: '--font-poppins',
-    weight: ["200", "400", "700"],
+    subsets: ["latin"], variable: '--font-poppins', weight: ["200", "400", "700"],
 })
 
 export default function RootLayout({children}) {
 
-    const pathname = usePathname()
 
-    return (
-        <html lang="en">
-        <body className={`${poppins.variable} font-sans`}>
-        <header>
-            <nav>
-                <Link className="nav-link" href="/">Home</Link>
-                <div>
-                    <Link className="nav-link" href="/register">Register</Link>
-                    <Link className="nav-link" href="/dashboard">Dashboard</Link>
-                </div>
-            </nav>
-        </header>
+    return (<html lang="en">
+    <body className={`${poppins.variable} font-sans`}>
+    <header>
+        <Navigation/>
+    </header>
 
-        <main>
-            {children}
-        </main>
+    <main>
+        {children}
+    </main>
 
-        <footer>
-            Footer
-        </footer>
-        </body>
-        </html>
-    );
+    <Footer />
+    </body>
+    </html>);
 }
