809ca1f8f9c65b909cdb3e33f1edc63ede9045d2
TinnTinnn <144338656+TinnTinnn@users.noreply.github.com>
2025-03-07 15:24:50 +0700

    Create middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,33 @@
+import getAuthUser from "@/lib/getAuthUser";
+import {NextResponse} from "next/server";
+
+const protectedRoutes = ["/dashboard", "/members/create"]
+const publicRoutes = ["/login", "/register"]
+
+export default async function middleware(req) {
+
+    const path = req.nextUrl.pathname;
+    const isProtected =
+        protectedRoutes.includes(path) || path.startsWith("/members/edit");
+    const isPublic = publicRoutes.includes(path)
+
+    const user = await getAuthUser()
+    const userId = user?.userId
+
+    if (isProtected && !userId) {
+        return NextResponse.redirect(new URL('/login', req.nextUrl))
+    }
+
+    if (isPublic && userId) {
+        return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
+    }
+
+   return NextResponse.next()
+}
+
+export const config = {
+    // matcher: ["/dashboard", '/members/:path*'], // do with yourself
+    matcher: [   // Do with all next.js design for all routes
+        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    ],
+}
